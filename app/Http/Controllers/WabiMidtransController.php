6ba7289cd1efbd9ae5b38433f918a3ed69155c1b
Rozir Wobari <2rozirwobari@gmail.com>
2025-06-16 06:54:17 +0700

    Update Midtrans Controller

--- app/Http/Controllers/WabiMidtransController.php
+++ app/Http/Controllers/WabiMidtransController.php
@@ -19,10 +19,10 @@ class WabiMidtransController extends Controller
         $serverKey = config('midtrans.server_key');
         
         // Ambil data dari callback
-        $orderId = $request->order_id ?? $request->no_invoice;
-        $statusCode = $request->status_code ?? $request->data['status_code'];
-        $grossAmount = $request->gross_amount ?? $request->data['gross_amount'];
-        $signatureKey = $request->signature_key ?? $request->data['signature_key'];
+        $orderId = $request->order_id ?? $request->no_invoice ?? null;
+        $statusCode = $request->status_code ?? $request->data['status_code'] ?? null;
+        $grossAmount = $request->gross_amount ?? $request->data['gross_amount'] ?? null;
+        $signatureKey = $request->signature_key ?? $request->data['signature_key'] ?? null;
         
         // Verifikasi signature key
         // $expectedSignature = hash('sha512', $orderId . $statusCode . $grossAmount . $serverKey);
@@ -33,7 +33,7 @@ class WabiMidtransController extends Controller
         // }
         
         // Proses berdasarkan transaction status
-        $transactionStatus = $request->transaction_status ?? $request->data['no_invoice'];
+        $transactionStatus = $request->transaction_status ?? $request->data['no_invoice'] ?? null;
         $fraudStatus = ($request->fraud_status ?? $request->data['fraud_status']) ?? null;
         
         switch ($transactionStatus) {
