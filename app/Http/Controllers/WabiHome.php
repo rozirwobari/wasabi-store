870d16076f2663d3d6f4cd6f770b368747822206
Rozir Wobari <2rozirwobari@gmail.com>
2025-06-25 05:42:41 +0700

    Add Library respon dan Http

--- app/Http/Controllers/WabiHome.php
+++ app/Http/Controllers/WabiHome.php
@@ -3,8 +3,6 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Http\Client\Response;
-use Illuminate\Support\Facades\Http;
 
 use App\Models\ProdukModel;
 use App\Models\CartModel;
