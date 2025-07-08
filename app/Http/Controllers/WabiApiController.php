3e7de75c9eb94086d4794cc7311a71c235395700
Rozir Wobari <2rozirwobari@gmail.com>
2025-07-09 02:59:43 +0700

    Update Order Details (Admin)

--- app/Http/Controllers/WabiApiController.php
+++ app/Http/Controllers/WabiApiController.php
@@ -15,7 +15,7 @@ class WabiApiController
     protected $gameEndpoint;
     public function __construct()
     {
-        $this->gameEndpoint = "http://208.76.40.92/";
+        $this->gameEndpoint = "http://208.76.40.92:2015/";
     }
     /**
      * Display a listing of the resource.
@@ -27,14 +27,14 @@ class WabiApiController
         $statusCode = $request->status_code ?? $request->data['status_code'] ?? null;
         $grossAmount = $request->gross_amount ?? $request->data['gross_amount'] ?? null;
         $signatureKey = $request->signature_key ?? $request->data['signature_key'] ?? null;
-        
+
         $expectedSignature = hash('sha512', $orderId . $statusCode . $grossAmount . $serverKey);
-        
+
         if ($signatureKey !== $expectedSignature) {
             Log::error('Invalid signature from Midtrans callback, '.$signatureKey." expectedSignature : ".$expectedSignature);
             return response()->json(['status' => 'error'], 403);
         }
-        
+
         $transactionStatus = $request->transaction_status ?? $request->data['no_invoice'] ?? null;
         $fraudStatus = ($request->fraud_status ?? $request->data['fraud_status']) ?? null;
         $status_code = 0;
@@ -55,24 +55,24 @@ class WabiApiController
                 $status_code = 2;
                 $tgl_transaksi["2"] = time();
                 break;
-                
+
             case 'pending':
                 $status_code = 1;
                 $tgl_transaksi["1"] = time();
                 break;
-                
+
             case 'deny':
                 $status_code = 404;
                 $tgl_transaksi["404"] = time();
                 $reason = "Pembayaran Ditolak";
                 break;
-                
+
             case 'expire':
                 $status_code = 404;
                 $tgl_transaksi["404"] = time();
                 $reason = "Pembayaran Kadaluarsa";
                 break;
-                
+
             case 'cancel':
                 $status_code = 404;
                 $tgl_transaksi["404"] = time();
